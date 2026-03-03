1e468ef9d9363791235fa4b02d2726d44f91bcd7
bugrahan14 <ertasbugra0611@gmail.com>
2026-03-03 13:59:21 +0300

    custom login form add

---
+++ src/main/java/com/bugrahan/company/demosecurity/controller/loginController.java
@@ -0,0 +1,16 @@
+package com.bugrahan.company.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class loginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String  showMyLoginPage(){
+
+        return "plain-login";
+    }
+
+
+}
